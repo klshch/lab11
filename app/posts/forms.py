d5bb9dbe323a468e5ea702208b7f7e923c1db3db
Anastasiia Klishch <147326209+klshch@users.noreply.github.com>
2023-12-04 23:59:50 +0200

    part 1

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -21,3 +21,21 @@ class CreatePost(FlaskForm):
     submit = SubmitField('Create',
                          render_kw={"class": "btn btn-primary"})  
     
+
+
+class EditPostForm(FlaskForm):
+    title = StringField('Title:',
+                        validators=[DataRequired("This field is required.")],
+                        render_kw={"class": "form-control"})
+    text = TextAreaField('Write your post:', 
+                         validators=[DataRequired("This field is required.")],
+                         render_kw={"class": "form-control"})
+    type = SelectField("Post type:", 
+                       validators=[DataRequired("Select a type.")],
+                       choices=[("news", "News"), ("publication", "Publication"), ("other", "Other")],
+                       render_kw={"class": "form-control", "multiple": True})
+    image_file = FileField('Change image:', 
+                           validators=[FileAllowed(['jpg', 'png'])],
+                           render_kw={"class": "form-control"})
+    submit = SubmitField('Submit',
+                         render_kw={"class": "btn btn-primary"})  
--- app/posts/views.py
+++ app/posts/views.py
@@ -5,6 +5,7 @@ from app import db
 from .models import Post
 
 from .forms import CreatePost
+from .forms import EditPostForm
 
 from .saver import save_picture
 
@@ -14,15 +15,16 @@ from . import posts
 @posts.route("/create", methods=['GET', 'POST'])
 @login_required
 def create():
-
     form = CreatePost()
+
+    image_file = None
 
     if form.validate_on_submit():
         title = form.title.data
         text = form.text.data
         type = form.type.data
         user_id = current_user.id
- 
+
         if form.image_file.data:
             image_file = save_picture(form.image_file.data)
 
@@ -33,9 +35,9 @@ def create():
 
         flash('Your post has been created!', 'success')
         return redirect(url_for('posts.create'))
-        
 
     return render_template('new_post.html', form=form)
+
 
 
 @posts.route('/post', methods=['GET'])
@@ -50,3 +52,48 @@ def post(id):
     post = Post.query.filter_by(id=id).first_or_404()
 
     return render_template("post.html", post=post, current_user=current_user)
+
+
+@posts.route("/post/<int:id>/edit", methods=['GET', 'POST'])
+def edit(id):
+    post = Post.query.get_or_404(id)
+    form = EditPostForm() 
+
+    if form.validate_on_submit():
+        if form.title.data != post.title or form.text.data != post.text or form.type.data != post.type:
+            post.title = form.title.data
+            post.text = form.text.data
+            post.type = form.type.data
+            db.session.commit()
+
+            flash("Post has been updated", "success")
+            return redirect(url_for('posts.post', id=post.id))
+        
+        if form.image_file.data:
+            post.image_file = save_picture(form.image_file.data)
+            db.session.commit()
+
+            flash("Post has been updated", "success")
+            return redirect(url_for('posts.post', id=post.id))
+        
+        else:
+            flash('No changes were made to your post.', 'info')
+            return redirect(url_for('posts.post', id=post.id))
+
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.text.data = post.text
+        form.type.data = post.type
+
+    return render_template('edit.html', post=post, form=form)
+
+
+@posts.route("/post/<int:id>/delete")
+def delete(id):
+    post = Post.query.filter_by(id=id).first_or_404()
+        
+    db.session.delete(post)
+    db.session.commit()
+    flash("Post has been deleted", "success")
+    return redirect(url_for('posts.all_posts'))
+
